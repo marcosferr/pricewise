5b692fe393aac47c8a30b0d0cea779d711ecfa66
marcosferr <ferreiramarcosadrian@gmail.com>
2024-02-23 07:14:18 -0300

    Add return type to GET function in cron route

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -13,7 +13,7 @@ import { NextResponse } from "next/server";
 export const maxDuration = 300; // 5 minutes
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
-export async function GET() {
+export async function GET(): Promise<any> {
   try {
     connectToDB();
     const products = await Product.find();
